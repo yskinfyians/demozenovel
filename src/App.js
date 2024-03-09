7794c429bf26e8ddf9c5879cd3dc233a7be8a23a
yskinfyians <yskinfy@gmail.com>
2024-03-10 00:52:43 +0530

    new addition

--- src/App.js
+++ src/App.js
@@ -1,56 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import {
+  createHashRouter, // Change import to use createHashRouter
+  createRoutesFromElements,
+  Route,
+  Routes,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+import LoginPage from "./loginPage/LoginPage";
+import { token } from "./redux/LoginReducer";
+import { useSelector } from "react-redux";
+import ProtectedRoute from "./protectedRoutes/ProtectedRoute";
+import Navbar from "./Navbar/Navbar";
+import { loginSelector } from "./redux/LoginReducer";
+import Home from "./Home/Home";
+import TrackerUI from "./ProjctTrack/TrackerUI";
+import ViewDesign from "./ViewTracker/ViewDesign";
+// import Navbar from "./Navbar/Navbar";
 
 function App() {
+  const selector = useSelector(loginSelector);
+
+  // const router = createHashRouter([
+  //   // Change to createHashRouter
+  //   {
+  //     path: "/",
+  //     element: <LoginPage />,
+  //     children: [
+  //       // { index: true, element: <Navbar /> },
+  //       {
+  //         index: true,
+  //         element: selector.token !== "" ? <Navbar /> : <LoginPage />,
+  //       },
+
+  //       // { path: "viewTracker", element: <ViewDesign /> },
+  //       // { path: "MapConfiguration", element: <AddMapConfiguration /> },
+  //     ],
+  //   },
+  // ]);
+
+  console.log(selector.token);
+  const loggedIn = selector.loggedIn;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={loggedIn ? <Navigate to="/home" /> : <LoginPage />}
+        />
+
+        <Route exact path="/" element={<ProtectedRoute />}>
+          <Route exact path="home" element={<Home />} />
+          <Route exact path="projectTracking" element={<TrackerUI />} />
+          <Route exact path="viewTracker" element={<ViewDesign />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>
 );
 
